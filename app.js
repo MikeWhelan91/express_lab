e18f1459aa351b56cfe87d1f277c15a47202b483
Michael Whelan <whelano07@hotmail.com>
2019-10-28 23:02:29 +0000

    changed app file

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello Michael!'))
+app.get('/', (_req, res) => res.send('Welcome to Data Representation & Querying!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
